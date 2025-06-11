1a9230beec0aac45c70748e2075ce27a0486d514
maxmax <174373891+gudovic@users.noreply.github.com>
2025-06-11 17:24:05 +0200

    textContent linguine e pepe

--- javascript.js
+++ javascript.js
@@ -7,36 +7,34 @@ buttons.forEach(button => button.addEventListener('click', () => {
 	let displayValue = display.textContent;
 	let buttonValue = button.textContent;
   	let firstString = smallDisplay.textContent;
-	// Need to fix this so it works on clicking symbols and dont return NaN
-	if (display.textContent === "Can't divide by zero") {display.textContent = ""; smallDisplay.textContent = ""}
-		//Number show up on click
+  if (display.textContent === 'Cant divide by zero') {smallDisplay.textContent = ''; display.textContent = ''; return;}
+	//Handle number inputs
 		if (!isNaN(buttonValue)) {
-		if (display.textContent == 0) {display.textContent = ""}
-		display.textContent = display.textContent + button.textContent;}
-		//Handle calculation
-   		else if ((button.textContent=='+'||button.textContent=='-'||button.textContent=='*'||button.textContent=='/') && displayValue !== "" && firstString !== "") {
-		if (firstString.includes('+')) {let result = (Number(displayValue)+Number(firstString.slice(0, -1))); smallDisplay.textContent = result + button.textContent; display.textContent = "";} 
-		else if (firstString.includes('-')) {let result = (Number(firstString.slice(0, -1))-Number(displayValue)); smallDisplay.textContent = result + button.textContent; display.textContent = "";}
-		else if (firstString.includes('*')) {let result = (Number(firstString.slice(0, -1))*Number(displayValue)); smallDisplay.textContent = result + button.textContent; display.textContent = "";}
-		else if (firstString.includes('/')) 
-			{if (Number(displayValue == 0)) {display.textContent = "Can't divide by zero"} 
-			else {let result = (Number(firstString.slice(0, -1))/Number(displayValue)); smallDisplay.textContent = result + button.textContent; display.textContent = "";}}
-
-
-	
-	}
-	
-		//Change operation when display i empty
+		if (display.textContent === '0') {display.textContent = ""} display.textContent = display.textContent + button.textContent;}
+	//Chain calculation
+    else if ((button.textContent=='+'||button.textContent=='-'||button.textContent=='*'||button.textContent=='/') && displayValue !== "" && firstString !== "") {
+    if (firstString.includes('+')) {smallDisplay.textContent = (Number(firstString.slice(0, -1)) + Number(display.textContent) + buttonValue); display.textContent = ""}
+    if (firstString.includes('-')) {smallDisplay.textContent = (Number(firstString.slice(0, -1)) - Number(display.textContent) + buttonValue); display.textContent = ""}
+    if (firstString.includes('*')) {smallDisplay.textContent = (Number(firstString.slice(0, -1)) * Number(display.textContent) + buttonValue); display.textContent = ""}
+    if (firstString.includes('/')) {smallDisplay.textContent = (Number(firstString.slice(0, -1)) / Number(display.textContent) + buttonValue); display.textContent = ""}
+    if (firstString.includes('/') && displayValue == '0') {display.textContent = 'Cant divide by zero'; smallDisplay.textContent = ''} 
+    }
     else if (button.textContent=='+'||button.textContent=='-'||button.textContent=='*'||button.textContent=='/') {
     	if (display.textContent === "") {smallDisplay.textContent = firstString.slice(0, -1) + button.textContent}
       	else smallDisplay.textContent = display.textContent + button.textContent;
         			display.textContent = "";}
-		//Function of weird buttons, need to fix comma bugg when you put 0.
-			else if (button.textContent=='Backspace') {display.textContent = displayValue.slice(0, -1) || '0'}
-    		else if (button.textContent=='C') {display.textContent = '0'; smallDisplay.textContent = ''}
-    		else if (button.textContent=='CE') {display.textContent = '0'}
-			else if (button.textContent=='.') {if (!display.textContent.includes('.')) display.textContent = display.textContent + button.textContent}
-
+		else if (button.textContent=='Backspace') {display.textContent = displayValue.slice(0, -1) || '0'}
+    	else if (button.textContent=='C') {display.textContent = '0'; smallDisplay.textContent = ''}
+    	else if (button.textContent=='CE') {display.textContent = '0'}
+		else if (button.textContent=='.') {if (!displayValue.includes('.')){display.textContent = display.textContent + button.textContent}}
+		else if (button.textContent=='=') {
+    if (firstString.includes('+')) {display.textContent = (Number(firstString.slice(0, -1)) + Number(display.textContent) ); smallDisplay.textContent = ""}
+    if (firstString.includes('-')) {display.textContent = (Number(firstString.slice(0, -1)) - Number(display.textContent) ); smallDisplay.textContent = ""}
+    if (firstString.includes('*')) {display.textContent = (Number(firstString.slice(0, -1)) * Number(display.textContent) ); smallDisplay.textContent = ""}
+    if (firstString.includes('/')) {display.textContent = (Number(firstString.slice(0, -1)) / Number(display.textContent) ); smallDisplay.textContent = ""}
+    if (firstString.includes('/') && displayValue == '0') {display.textContent = 'Cant divide by zero'; smallDisplay.textContent = ''} 
+    }
 }))
 }
 clickableBtn();
+
